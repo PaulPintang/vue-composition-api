264b76492d9cf7bc9c40732736d2597307355c13
puldyastin <puldyastin@gmail.com>
2022-02-14 22:20:04 +0800

    props in setup()

--- src/components/FetchData.vue
+++ src/components/FetchData.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>test</div>
+  <div>
+    <button @click="click"></button>
+    <Props name="Paul" />
+  </div>
 </template>
 
 <script>
+// import { methods } from "vue";
+import Props from "./Props.vue";
 export default {
   name: "FetchData",
+  components: { Props },
+  setup() {
+    const click = () => {
+      alert("sdsad");
+    };
+    return { click };
+  },
 };
 </script>
 
---
+++ src/components/Props.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>props component</div>
+</template>
+
+<script>
+export default {
+  name: "Props",
+  props: { name },
+  setup(props) {
+    console.log(props.name);
+  },
+};
+</script>
+
+<style></style>
